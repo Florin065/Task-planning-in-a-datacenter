b3d7942a56304c068b1545d072f92b22e0e542ed
Florin Ioan <subtirica.florin29@gmail.com>
2023-12-14 23:48:41 +0200

    Homework done. Need to add README

--- src/MyDispatcher.java
+++ src/MyDispatcher.java
@@ -1,14 +1,49 @@
-/* Implement this class. */
-
 import java.util.List;
 
 public class MyDispatcher extends Dispatcher {
-
+    private int nextHost = 0;
     public MyDispatcher(SchedulingAlgorithm algorithm, List<Host> hosts) {
         super(algorithm, hosts);
     }
 
+    /**
+     * Adds a task to the host with the least amount of work left.
+     * @param task the task to be added
+     */
     @Override
     public void addTask(Task task) {
+        switch (algorithm) {
+            case ROUND_ROBIN -> {
+                hosts.get(nextHost).addTask(task);
+                nextHost = (nextHost + 1) % hosts.size();
+            }
+            case SHORTEST_QUEUE -> {
+                int shortestQueue = 0;
+                for (int i = 1; i < hosts.size(); i++) {
+                    if (hosts.get(i).getQueueSize() < hosts.get(shortestQueue).getQueueSize()) {
+                        shortestQueue = i;
+                    }
+                }
+                hosts.get(shortestQueue).addTask(task);
+            }
+            case SIZE_INTERVAL_TASK_ASSIGNMENT -> {
+                switch (task.getType()) {
+                    case SHORT -> hosts.get(TaskType.SHORT.ordinal()).addTask(task);
+                    case MEDIUM -> hosts.get(TaskType.MEDIUM.ordinal()).addTask(task);
+                    case LONG -> hosts.get(TaskType.LONG.ordinal()).addTask(task);
+                    default -> throw new IllegalStateException("Unexpected value: " + task.getType());
+                }
+            }
+            case LEAST_WORK_LEFT -> {
+                int leastWorkLeft = 0;
+                for (int i = 1; i < hosts.size(); i++) {
+                    if (hosts.get(i).getWorkLeft() < hosts.get(leastWorkLeft).getWorkLeft()) {
+                        leastWorkLeft = i;
+                    }
+                }
+                hosts.get(leastWorkLeft).addTask(task);
+            }
+            default -> throw new IllegalStateException("Unexpected value: " + algorithm);
+        }
     }
 }
--- src/MyHost.java
+++ src/MyHost.java
@@ -1,25 +1,120 @@
-/* Implement this class. */
+import java.util.Comparator;
+import java.util.Objects;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MyHost extends Host {
+    private final PriorityBlockingQueue<Task> queue;
+    private volatile Task task;
+    private final Object lock;
+    private final AtomicBoolean isRunning;
+
+    /**
+     * Constructs a new host with a priority queue and a lock.
+     */
+    public MyHost() {
+        this.queue = new PriorityBlockingQueue<>(1, Comparator
+                .comparing(Task::getPriority)
+                .reversed()
+                .thenComparing(Task::getStart));
+        this.task = null;
+        this.lock = new Object();
+        this.isRunning = new AtomicBoolean(true);
+    }
+
+    /**
+     * Starts the host and performs tasks until it is shut down.
+     */
     @Override
     public void run() {
+        while (isRunning.get()) {
+            performTask();
+        }
     }
 
+    /**
+     * Performs the task if there is one and if it is not finished yet.
+     */
+    private void performTask() {
+        synchronized (lock) {
+            if (task == null) {
+                task = queue.poll();
+            } else {
+                executeTask();
+            }
+        }
+    }
+
+    /**
+     * Executes the task until it is finished or until it is preempted by a task with a higher priority in the queue.
+     */
+    private void executeTask() {
+        long initialWorkTime = System.currentTimeMillis();
+
+        while (task.getLeft() > 0) {
+            preemptTaskIfNecessary();
+
+            long timeWhenWorkDone = System.currentTimeMillis();
+            task.setLeft(task.getLeft() - (timeWhenWorkDone - initialWorkTime));
+            initialWorkTime = timeWhenWorkDone;
+        }
+
+        finishTask();
+    }
+
+    /**
+     * Preempts the task if there is one and if there is a task with a higher priority in the queue.
+     */
+    private void preemptTaskIfNecessary() {
+        if (task.isPreemptible()
+                && !queue.isEmpty()
+                && task.getPriority() < Objects.requireNonNull(queue.peek()).getPriority()) {
+            queue.add(task);
+            task = queue.poll();
+        }
+    }
+
+    /**
+     * Finishes the task.
+     */
+    private void finishTask() {
+        if (task != null) {
+            task.finish();
+            task = null;
+        }
+    }
+
+    /**
+     * Adds a task to the queue.
+     *
+     * @param task the task to add
+     */
     @Override
     public void addTask(Task task) {
+        queue.add(task);
     }
 
+    /**
+     * @return the size of the queue
+     */
     @Override
     public int getQueueSize() {
-        return 0;
+        return queue.size() + (task == null ? 0 : 1);
     }
 
+    /**
+     * @return the amount of work left to do
+     */
     @Override
     public long getWorkLeft() {
-        return 0;
+        return queue.stream().mapToLong(Task::getLeft).sum() + (task != null ? task.getLeft() : 0);
     }
 
+    /**
+     * Shuts down the host.
+     */
     @Override
     public void shutdown() {
+        isRunning.set(false);
     }
 }
